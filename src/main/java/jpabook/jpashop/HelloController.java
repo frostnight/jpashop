cd96a6df0ace56619a70c52a461a8bd13f0612e4
frostnight <frostnightsong@gmail.com>
2021-03-13 01:19:07 +0900

    210313

--- src/main/java/jpabook/jpashop/HelloController.java
+++ src/main/java/jpabook/jpashop/HelloController.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop;
 
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+
+import jpabook.jpashop.domain.Member;
+import org.junit.Test;
+
+class MemberServiceTest {
+
+    @Test
+    public void 회원가입() throws Exception{
+
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+
+        // then
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception{
+
+        // given
+
+        // when
+
+        // then
+    }
+
+
+
+}
